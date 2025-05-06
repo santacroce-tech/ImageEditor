30dbeb0ce1b09339a2f5880e9e5c79d8e55267e2
Roberto Santacroce <roberto@santacroce.xyz>
2025-05-07 00:06:16 +0200

    Initial Commit

---
+++ ImageEditor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageEditor
+//
+//  Created by Roberto Santacroce on 7/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageEditor/ImageEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageEditorApp.swift
+//  ImageEditor
+//
+//  Created by Roberto Santacroce on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
